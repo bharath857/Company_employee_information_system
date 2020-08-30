504fc232ebd52e9b7a82d945410d546b2005f7ad
BHARATH J <bharath.reddy.857@gmail.com>
2020-08-30 15:28:57 +0530

    Update Employees.js

--- Api/assinment/src/controllers/Employees.js
+++ Api/assinment/src/controllers/Employees.js
@@ -2,39 +2,32 @@ var express= require('express');
 var router = new express.Router()
 const url = require('url');
 const Employees = require('../models/Employees');
-const { db } = require('../models/Employees');
 
-
-router.post('/Employees', (req, res) => {
-    //req.body.Emp_Id
-    const employees = new Employees(req.body)
-    employees.save().then(() => {
-        res.send(employees)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-})
-
-router.post('/employee/login', async (req, res) => {
+router.post('/Employees/login', async (req, res) => {
     
     try{
-        console.log("in")
-        console.log(req.body)
-        const employee = await Employees.adminLogin(req.body.email, req.body.password)
-
+        const employee = await Employees.adminLogin(req.body)
         res.send(employee)
     } catch(error){
-        res.send(500).send()
-    }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+        res.status(400).send(error.message)
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 })
 
 router.get('/Employees',(req, res)=>{
     Employees.find({}).then((employees)=>{
         res.send(employees)
     }).catch((error)=>{
-        res.send(500).send()
+        res.status(500).send()
     })
+})
+
+router.post('/Employees', (req, res) => {
+    const employees = new Employees(req.body)
+    employees.save().then(() => {
+        res.send(employees)
+    }).catch((error) => {
+        res.status(400).send()
+    })                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 })
 
 router.get('/Employees/findbyid/:id',(req, res)=>{
@@ -45,7 +38,7 @@ router.get('/Employees/findbyid/:id',(req, res)=>{
         console.log(employees)
     }).catch((error)=>{
         console.log('No match')
-        res.send(500).send();
+        res.status(500).send();
     })
 }) 
 
@@ -53,10 +46,10 @@ router.get('/Employees/findbyempid/:Emp_Id',(req, res)=>{
     const Emp_Id = req.params
     Employees.find(Emp_Id).then((employees)=>{
         res.send(employees)
-        console.log(employees)
+       // console.log(employees)
     }).catch((error)=>{ 
         console.log('No match')
-        res.send(500).send();
+        res.status(500).send();
     })
 })
 
@@ -67,7 +60,7 @@ router.get('/Employees/findbyname/:Emp_Name',(req, res)=>{
         console.log(employees)
     }).catch((error)=>{ 
         console.log('No match')
-        res.send(500).send();
+        res.status(500).send();
     })
 })
 
@@ -78,45 +71,43 @@ router.get('/Employees/findbyphone/:phoneNumber',(req, res)=>{
         console.log(employees)
     }).catch((error)=>{ 
         console.log('No match')
-        res.send(500).send();
+        res.status(500).send();
     })
 })
 router.get('/Employees/findSubordinates/:RepotingTo',(req, res)=>{
     const report = req.params
+    console.log(report)
     Employees.find(report).then((employees)=>{
         res.send(employees)
         console.log(employees)
     }).catch((error)=>{ 
         console.log('No match')
-        res.send(500).send();
+        res.status(500).send();
     })
 })
 
-router.get('/Employees/findSup/:RepotingTo',(req, res)=>{
-    const report = req.params
-    Employees.find(report).then((employees)=>{
-        res.send(employees)
-        console.log(employees)
-    }).catch((error)=>{ 
-        console.log('No match')
-        res.send(500).send();
-    })
-})
 
 router.patch('/Employees/findbyid/:id', async (req, res)=>{
     const update = Object.keys(req.body)
     const updatescanbedone = ['Company_Name', 'Emp_Name', 'Emp_Id', 'phoneNumber', 'age', 'email', 'password','Role', 'Division', 'RepotingTo'];
 
     const validation = update.every((update)=> updatescanbedone.includes(update))
-
+ 
     if (!validation){
         return res.status(404).send({error:'invalid update'})
     }
+ 
     try{
-        const employee = await  Employees.findByIdAndUpdate(req.params.id, req.body, { new:true, runValidators:true})
+      //const employee = await  Employees.findByIdAndUpdate(req.params.id, req.body, { new:true, runValidators:true})
+
+       const employee = await  Employees.findById(req.params.id)
+        update.forEach((update) => {
+            employee[update] = req.body[update]
+        })
+        await employee.save();
         res.send(employee)
     } catch(error){
-        res.send(404).send()
+        res.status(404).send()
     }
 })
 
@@ -130,7 +121,7 @@ router.delete('/Employees/findbyid/:id', async (req, res)=>{
         res.send(employee)
         console.log(employee)
     } catch(error){
-        res.send(500).send()
+        res.status(500).send()
     }
 })
 
